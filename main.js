473831264206fc257e3e9e5b85873907d6eb6c3b
221fa04732 <140374924+221fa04732@users.noreply.github.com>
2024-08-20 12:05:59 +0530

    new main.js file added

--- main.js
+++ main.js
@@ -3,20 +3,25 @@ const col="black";
 
 function changeColour()
 {
-    let box_colour= document.getElementById("intro");
+    let intro= document.getElementById("intro");
     let box_colour= document.getElementById("box");
+    let button= document.getElementById("click_button");
+
     if(box_colour.style.backgroundColor===col)
     {
         box_colour.style.backgroundColor="white";
         box_colour.style.color="black";
-        intro.textContent= "This is my simple introduction";
+        intro.textContent= "INTRODUCTION";
+        button.textContent= "REVEAL";
+        button.style.color="black";
     }
     else 
     {
         box_colour.style.backgroundColor="black";
         box_colour.style.color="white";
-        intro.textContent= "My name is MRITYUNJAY KUMAR";
-    }
-    
+        intro.textContent= "MRITYUNJAY KUMAR";
+        button.textContent= "HIDE";
+        button.style.color="white";
+    }  
 }
 
