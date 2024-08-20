1c152f656c0dfa21d4a59f14aa551c1c025d6469
221fa04732 <140374924+221fa04732@users.noreply.github.com>
2024-08-20 10:25:54 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,16 +3,19 @@ const col="black";
 
 function changeColour()
 {
+    let box_colour= document.getElementById("intro");
     let box_colour= document.getElementById("box");
     if(box_colour.style.backgroundColor===col)
     {
         box_colour.style.backgroundColor="white";
         box_colour.style.color="black";
+        intro.textContent= "This is my simple introduction";
     }
     else 
     {
         box_colour.style.backgroundColor="black";
         box_colour.style.color="white";
+        intro.textContent= "My name is MRITYUNJAY KUMAR";
     }
     
 }
